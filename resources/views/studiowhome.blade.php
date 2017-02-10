99e2a3b55b2356598ae39d535bb5687c72fd78e3
Isuru Walpola <iwalpola@studiow.cf>
2017-02-10 09:39:25 +0000

    initial commit

---
+++ resources/views/studiowhome.blade.php
@@ -0,0 +1,574 @@
+<!DOCTYPE html>
+<!--[if lt IE 8 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 8)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+
+   <!--- Basic Page Needs
+   ================================================== -->
+   <meta charset="utf-8">
+   <title>Studio Walpola</title>
+   <meta name="description" content="Studio Walpola - Elegant Web and Product Design">
+   <meta name="author" content="Isuru Walpola">
+
+   <!-- Mobile Specific Metas
+  ================================================== -->
+   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+   <!-- CSS
+   ================================================== -->
+   <link rel="stylesheet" href="css/base.css">
+   <link rel="stylesheet" href="css/layout.css">
+
+   <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+   <![endif]-->
+
+   <!-- Favicons
+   ================================================== -->
+   <link rel="shortcut icon" href="favicon.ico" >
+
+
+</head>
+
+<body data-spy="scroll" data-target="#nav-wrap">
+
+
+   <!-- Header
+   ================================================== -->
+   <header class="mobile">
+
+      <div class="row">
+
+         <div class="col full">
+
+            <div class="logo">
+               <a href="#"><img alt="" src="images/logo.png"></a>
+            </div>
+
+            <nav id="nav-wrap">
+
+               <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+	            <a class="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+
+               <ul id="nav" class="nav">
+	               <li><a href="#intro">Home</a></li>
+                  <li><a href="#portfolio">Portfolio</a></li>
+                  <li><a href="#about">About Us</a></li>
+                  <li><a href="#contact">Contact</a></li>
+                  <li><a href="blog">BLOG</a></li>
+               </ul>
+
+            </nav>
+
+         </div>
+
+      </div>
+
+   </header> <!-- Header End -->
+
+
+   <!-- Intro Section
+   ================================================== -->
+   <section id="intro">
+
+      <!-- Flexslider Start-->
+	   <div id="intro-slider" class="flexslider">
+
+		   <ul class="slides">
+
+			   <!-- Slide -->
+			   <li>
+				   <div class="row">
+					   <div class="col full">
+						   <div class="slider-text">
+							   <h2>Where inspired <span>design</span> meets solid <span>engineering</span>. Check out our <a style="text-decoration: underline;" href="#portfolio" title="">portfolio</a>.</h2>
+						   </div>
+					   </div>
+				   </div>
+			   </li>
+
+            <!-- Slide -->
+			   <li>
+				   <div class="row">
+					   <div class="col full">
+						   <div class="slider-text">
+							   <h2>We specialize in consumer <span>web services </span>and <span>product design</span>, combined with <span>electronics</span> developed in-house.</h2>
+						   </div>
+					   </div>
+				   </div>
+			   </li>
+
+		   </ul>
+	   </div>
+	   <!-- Flexslider End-->
+
+   </section> <!-- Intro Section End-->
+
+
+   <!-- Portfolio Section
+   ================================================== -->
+   <section id="portfolio">
+
+      <div class="row section-head">
+         <div class="col full">
+
+            <h2>Portfolio</h2>
+            <p class="desc">Check out our latest projects.</p>
+
+            <p class="intro">We prototype and build web services, consumer products, and in-house electronics.
+            </p>
+
+         </div>
+      </div>
+
+      <div class="row">
+
+		   <!-- Portfolio Wrapper -->
+		   <div id="portfolio-wrapper">
+
+            <div class="col portfolio-item">
+               <div class="item-wrap">
+                  <a href="#" data-reveal-id="modal-06"><img src="images/portfolio/wbook.jpg" alt=""/></a>
+                  <div class="portfolio-item-meta">
+                     <h5><a href="#">Audiovisual Tutor</a></h5>
+                  </div>
+               </div>
+            </div>
+			   
+            <div class="col portfolio-item">
+               <div class="item-wrap">
+                  <a href="#" data-reveal-id="modal-01"><img src="images/portfolio/moduvape.jpg" alt=""/></a>
+                  <div class="portfolio-item-meta">
+                     <h5><a href="#">Modular Vape (Concept)</a></h5>
+                  </div>
+               </div>
+            </div>
+
+            <div class="col portfolio-item">
+               <div class="item-wrap">
+                  <a href="#" data-reveal-id="modal-02"><img src="images/portfolio/cohi.jpg" alt=""/></a>
+                  <div class="portfolio-item-meta">
+                     <h5><a href="#">Children of Hope</a></h5>
+                  </div>
+               </div>
+            </div>
+
+            <div class="col portfolio-item">
+               <div class="item-wrap">
+                  <a href="#" data-reveal-id="modal-03"><img src="images/portfolio/pentaheater.jpg" alt=""/></a>
+                  <div class="portfolio-item-meta">
+                     <h5><a href="#">Pentaheater (Prototype)</a></h5>
+                  </div>
+               </div>
+            </div>
+
+            <div class="col portfolio-item">
+               <div class="item-wrap">
+					   <a href="#" data-reveal-id="modal-04"><img src="images/portfolio/eviolin.jpg" alt=""/></a>
+					   <div class="portfolio-item-meta">
+						   <h5><a href="#">Electric Violin</a></h5>
+					   </div>
+               </div>
+			   </div>
+
+            <div class="col portfolio-item">
+               <div class="item-wrap">
+					   <a href="#" data-reveal-id="modal-05"><img src="images/portfolio/bitsign.jpg" alt=""/></a>
+					   <div class="portfolio-item-meta">
+						   <h5><a href="#">BitSign.it (development)</a></h5>
+					   </div>
+               </div>
+			   </div>
+
+		   </div> <!-- Portfolio Wrapper End -->
+
+
+		</div> <!-- End Row -->
+
+
+      <!-- Modal Popup
+	   =========================================================== -->
+
+      <!-- modal-01 -->
+	   <div id="modal-01" class="reveal-modal">
+
+         <img class="scale-with-grid" src="images/portfolio/modals/moduvape.jpg" alt="" />
+
+         <div class="description-box">
+            <h4>Modular Vaporizer</h4>
+            <p>The World's first Upgradable and Reconfigurable Herbal Vaporizer.</p>
+            <span class="categories"><i class="icon-tag"></i>CAD, Product Design</span>
+         </div>
+
+         <div class="link-box">
+            <a href="http://www.moduvape.com">Details</a>
+            <a class="close-reveal-modal">Close</a>
+         </div>
+
+      </div><!-- modal-01 End -->
+
+      <!-- modal-02 -->
+	   <div id="modal-02" class="reveal-modal">
+
+		   <img class="scale-with-grid" src="images/portfolio/modals/cohi.jpg" alt="" />
+
+		   <div class="description-box">
+			   <h4>Children of Hope India Website</h4>
+			   <p>[Project done au gratis]
+            We built/modified the branding of the organization, organized content, adapted a theme and integrated social media features. Back-end (PHP) server side management, CSS3 and HTML were core skill areas applied.</p>
+            <span class="categories"><i class="icon-tag"></i>PHP, Wordpress, Web Design</span>
+         </div>
+
+         <div class="link-box">
+            <a href="http://www.childrenofhopeindia.org">Details</a>
+		      <a class="close-reveal-modal">Close</a>
+         </div>
+
+	   </div> <!-- modal-02 End -->
+
+      <!-- modal-03 -->
+	   <div id="modal-03" class="reveal-modal">
+
+		   <img class="scale-with-grid" src="images/portfolio/modals/pentaheater.jpg" alt="" />
+
+		   <div class="description-box">
+			   <h4>Pentaheater</h4>
+			   <p>Inline air heater that maintains constant heating by using semiconductor heating elements. It features an annular cathode and anode array which sandwich the heating elements.</p>
+            <span class="categories"><i class="icon-tag"></i>Engineering, CAD/Simulation, Design & Fab</span>
+         </div>
+
+         <div class="link-box">
+            <a href="http://www.studiowalpola.cf">Details</a>
+		      <a class="close-reveal-modal">Close</a>
+         </div>
+
+	   </div> <!-- modal-03 End -->
+
+      <!-- modal-04 -->
+	   <div id="modal-04" class="reveal-modal">
+
+		   <img class="scale-with-grid" src="images/portfolio/modals/eviolin.jpg" alt="" />
+
+		   <div class="description-box">
+			   <h4>Headless Electric Violin</h4>
+			   <p>Apart from it's excellent sound, this is a design that vastly improves the comfort level of the player's neck. This is done by shifting the weight of the violin towards the mid-point of the instrument.</p>
+            <span class="categories"><i class="icon-tag"></i>Design, Component Sourcing</span>
+         </div>
+
+         <div class="link-box">
+            <a href="http://www.instructables.com/id/Headless-Electric-Violin/">Details</a>
+		      <a class="close-reveal-modal">Close</a>
+         </div>
+
+	   </div> <!-- modal-04 End -->
+
+      <!-- modal-05 -->
+	   <div id="modal-05" class="reveal-modal">
+
+		   <img class="scale-with-grid" src="images/portfolio/modals/bitsign.jpg" alt="" />
+
+		   <div class="description-box">
+			   <h4>BitSign.it</h4>
+			   <p>Intellectual Property and Contracts on the Blockchain.</p>
+            <span class="categories"><i class="icon-tag"></i>PHP, Web Design</span>
+         </div>
+
+         <div class="link-box">
+            <a href="http://www.studiowalpola.cf">Details</a>
+		      <a class="close-reveal-modal">Close</a>
+         </div>
+
+	   </div> <!-- modal-05 End -->
+
+      <!-- modal-06 -->
+	   <div id="modal-06" class="reveal-modal">
+
+		   <img class="scale-with-grid" src="images/portfolio/modals/wbook.jpg" alt="" />
+
+		   <div class="description-box">
+			   <h4>Audiovisual Tutor Book</h4>
+			   <p>A low-cost yet immersive and effective English course. Course content is supported by audio (earphones)
+            and interactivity via the built in touchscreen.
+            Target cost: 700-1000 INR
+            Protected by these expired patents:
+            <a href="https://patents.google.com/patent/US5631883A/">Link</a>
+            <a href="https://patents.google.com/patent/US5290190A/">Link</a>
+            <a href="https://patents.google.com/patent/US5453013A/">Link</a>
+            <a href="https://patents.google.com/patent/US6064855A/">Link</a>
+
+            </p>
+            <span class="categories"><i class="icon-tag"></i>Electronics, Product Design</span>
+         </div>
+
+         <div class="link-box">
+            <a href="http://www.studiowalpola.cf">Details</a>
+		      <a class="close-reveal-modal">Close</a>
+         </div>
+
+	   </div> <!-- modal-06 End -->
+
+      <!-- modal-07 -->
+	   <div id="modal-07" class="reveal-modal">
+
+		   <img class="scale-with-grid" src="images/portfolio/modals/m-clock.jpg" alt="" />
+
+		   <div class="description-box">
+			   <h4>Clock</h4>
+			   <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
+            <span class="categories"><i class="icon-tag"></i>Branding, Web Design</span>
+         </div>
+
+         <div class="link-box">
+            <a href="http://www.studiowalpola.cf">Details</a>
+		      <a class="close-reveal-modal">Close</a>
+         </div>
+
+	   </div> <!-- modal-07 End -->
+
+      <!-- modal-08 -->
+	   <div id="modal-08" class="reveal-modal">
+
+		   <img class="scale-with-grid" src="images/portfolio/modals/m-fields.jpg" alt="" />
+
+		   <div class="description-box">
+			   <h4>Fields</h4>
+			   <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
+            <span class="categories"><i class="icon-tag"></i>Photography</span>
+         </div>
+
+         <div class="link-box">
+            <a href="http://www.studiowalpola.cf">Details</a>
+		      <a class="close-reveal-modal">Close</a>
+         </div>
+
+	   </div> <!-- modal-08 End -->
+
+   </section> <!-- Portfolio End -->
+
+
+   <!-- About Section
+   ================================================== -->
+   <section id="about">
+
+      <div class="row section-head">
+
+         <div class="col one-fourth">
+            <h2>About Us</h2>
+            <p class="desc">We're based in Colombo, Sri Lanka.</p>
+         </div>
+
+         <div class="col three-fourths">
+             <p class="intro">StudioW was formed by a group of friends and relatives, in order to bring the spirit of design and innovation to Sri Lanka, and other developing regions. We bring together people from different expertise areas (design, engineering, marketing, management) to confidently tackle product design challenges.</p>
+         </div>
+
+      </div>
+
+      <div class="row">
+         <div class="col full"><h3>Our Work Process.</h3></div>
+      </div>
+
+      <div class="row process-wrap">
+
+         <div class="col">
+            <h4>Explore.</h4>
+
+            <p>Our first step in any project is to get an idea of the existing technology and to explore missing links in the user experience. Another critical question is: "Can we do this?"</p>
+			</div>
+
+         <div class="col">
+            <h4>Design.</h4>
+
+            <p>After brainstrorming some sketches, we use Computer Aided Design (CAD) software to quickly make initial design choices. We then go back and forth balancing material availability, cost and functionality to optimize this design.
+            </p>
+			</div>
+
+         <div class="col m-first">
+            <h4>Prototype.</h4>
+
+            <p>Our engineers build a prototype according to the design that was planned, including metal/wood working, electronics prototyping and finishing.</p>
+			</div>
+
+         <div class="col">
+            <h4>Test & Deliver.</h4>
+
+            <p>We aren't satisfied if the customer isn't. We extensively test our product on potential customers, using their feedback to continually improve ou product (in the case of web services), or develop the next vesion(in case of products).
+            </p>
+			</div>
+
+      </div> <!-- Process Wrap End -->
+
+      <div class="row">
+         <div class="col full"><h3>Meet The Team.</h3></div>
+      </div>
+
+
+      <!-- Team Wrap End -->
+      <div class="row team-wrap">
+
+         <div class="col one-fourth">
+
+            <img src="images/team/team-img-01.jpg" alt=""/>
+
+            <div class"member-name">
+               <h5>Isuru</h5>
+               <span>Head Hacker</span>
+            </div>
+
+            <ul class="member-social">
+               <li><a href="http://github.com/iwalpola"><i class="icon-github"></i></a></li>
+               <li><a href="http://www.instructables.com/member/Half-Life/"><i class="icon-gears"></i></a></li>
+               <li><a href="http://www.linkedin.com/in/iwalpola"><i class="icon-linkedin"></i></a></li>
+            </ul>
+
+			</div>
+
+         <div class="col one-fourth">
+
+            <img src="images/team/team-img-02.jpg" alt=""/>
+
+            <div class"member-name">
+               <h5>Charani</h5>
+               <span>Design Lead</span>
+            </div>
+
+            <ul class="member-social">
+               <li><a href="#"><i class="icon-facebook"></i></a></li>
+               <li><a href="#"><i class="icon-twitter"></i></a></li>
+               <li><a href="#"><i class="icon-google-plus-sign"></i></a></li>
+               <li><a href="#"><i class="icon-linkedin"></i></a></li>
+               <li><a href="#"><i class="icon-skype"></i></a></li>
+            </ul>
+
+			</div>
+
+         <div class="col one-fourth">
+
+            <img src="images/team/team-img-03.jpg" alt=""/>
+
+            <div class"member-name">
+               <h5>Rohan</h5>
+               <span>Operations</span>
+            </div>
+
+            <ul class="member-social">
+               <li><a href="#"><i class="icon-facebook"></i></a></li>
+               <li><a href="#"><i class="icon-twitter"></i></a></li>
+               <li><a href="#"><i class="icon-google-plus-sign"></i></a></li>
+               <li><a href="#"><i class="icon-linkedin"></i></a></li>
+               <li><a href="#"><i class="icon-skype"></i></a></li>
+            </ul>
+
+			</div>
+
+      </div> <!-- Team Wrap End -->
+
+   </section> <!-- About Section End-->
+
+    <!-- Contact Section
+   ================================================== -->
+   <section id="contact">
+
+      <div class="row section-head">
+         <div class="col full">
+            <h2>Contact Us</h2>
+            <p class="desc">Get in touch with us.</p>
+         </div>
+      </div>
+
+      <div class="row">
+
+         <div class="col g-7">
+
+            <!-- form -->
+            <form name="contactForm" id="contactForm" method="post" action="">
+					<fieldset>
+
+                  <div>
+						   <label for="contactName">Name <span class="required">*</span></label>
+						   <input name="contactName" type="text" id="contactName" size="35" value="" />
+                  </div>
+
+                  <div>
+						   <label for="contactEmail">Email <span class="required">*</span></label>
+						   <input name="contactEmail" type="text" id="contactEmail" size="35" value="" />
+                  </div>
+
+                  <div>
+						   <label for="contactSubject">Subject</label>
+						   <input name="contactSubject" type="text" id="contactSubject" size="35" value="" />
+                  </div>
+
+                  <div>
+                     <label  for="contactMessage">Message <span class="required">*</span></label>
+                     <textarea name="contactMessage"  id="contactMessage" rows="15" cols="50" ></textarea>
+                  </div>
+
+                  <div>
+                     <button class="submit">Submit</button>
+                     <span id="image-loader">
+                        <img src="images/loader.gif" alt="" />
+                     </span>
+                  </div>
+
+					</fieldset>
+				</form> <!-- Form End -->
+
+            <!-- contact-warning -->
+            <div id="message-warning"></div>
+            <!-- contact-success -->
+				<div id="message-success">
+               <i class="icon-ok"></i>Your message was sent, thank you!<br />
+				</div>
+
+         </div>
+
+
+         <aside class="col g-5">
+
+            <h3>Contact Information</h3>
+
+            <p>
+            Studio Walpola <br />
+            14/5, Pangiriwatta Mawatha <br />
+            Mirihana, Nugegoda,
+            Colombo,
+            SRI LANKA
+            </p>
+
+            <p>
+            Phone: +917086880253 <br />
+            Email: info [@] studiowalpola.com
+            </p><br />
+
+         </aside>
+
+      </div>
+
+   </section> <!-- Contact Section End-->
+
+   <!-- footer
+   ================================================== -->
+   <footer>
+
+      <div class="row">
+
+      </div>
+
+   </footer> <!-- Footer End-->
+
+   <!-- Java Script
+   ================================================== -->
+   <script src="js/jquery-1.10.2.min.js"></script>
+   <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+
+   <script src="js/scrollspy.js"></script>
+   <script src="js/jquery.flexslider.js"></script>
+   <script src="js/jquery.reveal.js"></script>
+   <script src="js/init.js"></script>
+   <script src="js/smoothscrolling.js"></script>
+
+</body>
+
+</html>
