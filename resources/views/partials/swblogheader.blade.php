7706ef147c5400041552be63c145e0b3b3a9e115
Isuru Walpola <iwalpola@studiow.cf>
2017-02-12 00:16:35 +0530

    added header to blog

---
+++ resources/views/partials/swblogheader.blade.php
@@ -0,0 +1,27 @@
+<header class="mobile">
+
+      <div class="row">
+
+         <div class="col full">
+
+            <div class="logo">
+               <a href="#"><img alt="" src="images/logo.png"></a>
+            </div>
+
+            <nav id="nav-wrap">
+
+               <ul id="nav" class="nav">
+                   <li><a href="#intro">Home</a></li>
+                  <li><a href="#portfolio">Portfolio</a></li>
+                  <li><a href="#about">About Us</a></li>
+                  <li><a href="#contact">Contact</a></li>
+                  <li><a href="blog">BLOG</a></li>
+               </ul>
+
+            </nav>
+
+         </div>
+
+      </div>
+
+   </header>
