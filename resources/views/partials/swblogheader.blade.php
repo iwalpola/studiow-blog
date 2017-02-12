0a4f4cbe4aeed416b589c070cae5f732e8cc8fd1
Isuru Walpola <iwalpola@studiow.cf>
2017-02-12 19:22:11 +0530

    header fixes

--- resources/views/partials/swblogheader.blade.php
+++ resources/views/partials/swblogheader.blade.php
@@ -5,7 +5,7 @@
          <div class="col full">
 
             <div class="logo">
-               <a href="#"><img alt="" src="/images/logo.png"></a>
+               <a href="/"><img alt="" src="/images/logo.png"></a>
             </div>
 
             <nav id="nav-wrap">
@@ -15,7 +15,7 @@
                   <li><a href="/#portfolio">Portfolio</a></li>
                   <li><a href="/#about">About Us</a></li>
                   <li><a href="/#contact">Contact</a></li>
-                  <li><a href="blog">BLOG</a></li>
+                  <li class="active"><a href="blog">BLOG</a></li>
                </ul>
 
             </nav>
